73b7190ae95a4a2e8c337000a458e7ebc734d193
Bhuvi-github <64419633+Bhuvi-github@users.noreply.github.com>
2022-02-13 12:48:32 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+
+var ball;
+function setup() {
+  createCanvas(600, 600);
+  ball = createSprite(300,300,30,30);
+  
+}
+
+function draw() {
+  background("black");
+  //*console.log("hello javaScript");
+  drawSprites();
+}
+
